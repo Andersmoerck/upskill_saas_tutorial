53228744df83b8ceae4dff80f585cc796e834de9
Anders Moerck <anders@goboat.dk>
2018-02-19 07:05:30 +0000

    Added create action to save to db

--- saasapp/app/controllers/contacts_controller.rb
+++ saasapp/app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
     def new
+        @contact = Contact.new #creates a new blank object and stores the values within an instance variable @contact
     end
+
+    def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            redirect_to new_contact_path, notice: "Message sent."
+        else
+            redirect_to new_contact_path, notice: "Error occured."
+        end
+    end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
